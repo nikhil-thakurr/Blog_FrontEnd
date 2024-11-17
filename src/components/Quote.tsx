ca96e56992c511237db4434d2fcd28fb4c8db7de
Nikhil Thakur <nt34542@gmail.com>
2024-11-17 19:36:18 +0530

    All Code

---
+++ src/components/Quote.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Quote = () => {
+  return (
+    <div className='bg-slate-200 h-screen flex flex-col  justify-center items-center'>
+
+        <div className='text-3xl font-extrabold w-1/2'>
+            "The customer service i received was exceptional .The support team went above and beyond to address my concerns."
+        </div>
+        <div className=' font-bold mt-4 w-1/2'>
+            Jules Winnfield
+        </div>
+        <div className=' text-slate-500 w-1/2'>
+            CEO,Acme Corp
+        </div>
+
+    </div>
+  )
+}
+
+export default Quote
